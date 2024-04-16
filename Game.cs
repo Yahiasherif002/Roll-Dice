a14fae9000344381609c74d6de16da58747eba16
Yahia Ibrahim <Yahiasheriif87@gmail.com>
2024-04-16 10:19:27 +0200

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,51 @@
+﻿internal class Game
+{
+    private int numberOfPlayers;
+    private int numberOfDices;
+    private int numberOfRounds;
+    private int numberOfSides=6;
+    private int pointsToWin;
+
+    public Game(int numberOfPlayers, int numberOfDices, int numberOfRounds, int pointsToWin)
+    {
+        this.numberOfPlayers = numberOfPlayers;
+        this.numberOfDices = numberOfDices;
+        this.numberOfRounds = numberOfRounds;
+        this.pointsToWin = pointsToWin;
+    }
+    public void Start()
+    {
+       Random random=new Random();
+        int[] scores =new int[numberOfPlayers];
+        for (int round = 0; round < numberOfRounds; round++)
+        {
+            Console.WriteLine($"Round {round+1}");
+            Thread.Sleep(1000);
+            for (int player = 0; player < numberOfPlayers; player++)
+            {
+                int Totalscore = 0;
+                for (int k = 0; k < numberOfDices; k++)
+                {
+                    Console.WriteLine($"Player {player+1} rolling the {k+1} dice");
+                    Thread.Sleep(500);
+                    Totalscore += random.Next(1, numberOfSides + 1);
+                }
+                scores[player] += Totalscore;
+                Console.WriteLine($"Player {player+1} rolled {Totalscore}");
+                Thread.Sleep(1000);
+            }
+            
+        }
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            Console.WriteLine($"Player {i + 1} score : {scores[i]}");
+            if (scores[i] >= pointsToWin)
+            {
+                Console.WriteLine($"Player {i + 1} wins!");
+                return; // Exit the method if a player wins
+            }
+        }
+
+        Console.WriteLine("No player reached the winning score");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Welcome to the Roll-Dice game!");
+        Console.WriteLine("Press any key to start the game...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.WriteLine("Enter the number of players: ");
+        int numberOfPlayers = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of dices: ");
+        int numberOfDices = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of rounds: ");
+        int numberOfRounds = int.Parse(Console.ReadLine());
+       
+        Console.WriteLine("Enter the number of points to win: ");
+        int pointsToWin = int.Parse(Console.ReadLine());
+        Console.Clear();
+        Game game = new Game(numberOfPlayers, numberOfDices, numberOfRounds,  pointsToWin);
+        game.Start();
+        Console.WriteLine("Do you want to play again? (Y/N)");
+        string answer = Console.ReadLine().ToLower();
+        if (answer == "y")
+        {
+            Main(args);
+        }
+        else
+        {
+            Console.WriteLine("Thank you for playing!");
+        }
+
+
+    }
+}
